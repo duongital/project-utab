af26273f058d539838ad2589134044063cd7c6d1
duongital <duongital@gmail.com>
2025-04-19 20:03:07 +0700

    Refactor code structure for improved readability and maintainability

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,24 +1,99 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
-import '@pages/newtab/Newtab.css';
+import {
+  Geometry2d,
+  RecordProps,
+  Rectangle2d,
+  resizeBox,
+  T,
+  Tldraw,
+  TLResizeInfo,
+} from "tldraw";
+import "tldraw/tldraw.css";
+import { TLBaseShape } from "tldraw";
+import { HTMLContainer, ShapeUtil } from "tldraw";
+
+// [1]
+type ICustomShape = TLBaseShape<
+  "my-custom-shape",
+  {
+    w: number;
+    h: number;
+    text: string;
+  }
+>;
+
+// [2]
+export class MyShapeUtil extends ShapeUtil<ICustomShape> {
+  // [a]
+  static override type = "my-custom-shape" as const;
+  static override props: RecordProps<ICustomShape> = {
+    w: T.number,
+    h: T.number,
+    text: T.string,
+  };
+
+  // [b]
+  getDefaultProps(): ICustomShape["props"] {
+    return {
+      w: 200,
+      h: 200,
+      text: "I'm a shape!",
+    };
+  }
+
+  // [c]
+  override canEdit() {
+    return false;
+  }
+  override canResize() {
+    return true;
+  }
+  override isAspectRatioLocked() {
+    return false;
+  }
+
+  // [d]
+  getGeometry(shape: ICustomShape): Geometry2d {
+    return new Rectangle2d({
+      width: shape.props.w,
+      height: shape.props.h,
+      isFilled: true,
+    });
+  }
+
+  // [e]
+  override onResize(shape: any, info: TLResizeInfo<any>) {
+    return resizeBox(shape, info);
+  }
+
+  // [f]
+  component(shape: ICustomShape) {
+    return (
+      <HTMLContainer style={{ backgroundColor: "#efefef" }}>
+        {shape.props.text}
+      </HTMLContainer>
+    );
+  }
+
+  // [g]
+  indicator(shape: ICustomShape) {
+    return <rect width={shape.props.w} height={shape.props.h} />;
+  }
+}
+
+// [3]
+const customShape = [MyShapeUtil];
 
 export default function Newtab() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/newtab/Newtab.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+    <div style={{ position: "fixed", inset: 0 }}>
+      <Tldraw
+        persistenceKey="example"
+        shapeUtils={customShape}
+        onMount={(editor) => {
+          // editor.createShape({ type: "my-custom-shape", x: 100, y: 100 });
+          editor.focus();
+        }}
+      />
     </div>
   );
 }
